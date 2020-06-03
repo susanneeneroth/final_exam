54366e01aec75feeee1dab2785f98266073a35dc
susene <susanne.eneroth@centimedia.se>
2020-06-03 17:02:36 +0200

    final commit - not ready

--- src/main.js
+++ src/main.js
@@ -1,26 +1,19 @@
 new Vue({
-
   el: "#myapp",
   data: {
-    apiKey: '2f5c977f1fe4c5d58ff8c30431e6d7c8',
-    // searchButton: '',
     searchValue: '',
     posts: [],
-    query: ''
+    query: '',
+    showModal: ''
   },
 
-
-  // computed: {
-  //   filteredMovies: function () {
-  //     return this.movies.filter((movie) => {
-  //       let lowerMovie = movie.toLowerCase();
-  //       let lowerSearchValue = this.searchValue.toLowerCase();
-  //       return lowerMovie.includes(lowerSearchValue);
-  //     });
-  //   }
-  // },
-
-
+  computed: {
+    getPopularMovies() {
+      return this.posts.filter(posts => {
+        return posts.title.toLowerCase().includes(this.searchValue.toLowerCase())
+      })
+    }
+  },
 
   watch: {
     searchValue: function (value, oldValue) {
@@ -28,39 +21,64 @@ new Vue({
     }
   },
 
-  methods: {
-    getPopularMovies() {
-      fetch("https://api.themoviedb.org/3/search/movie?api_key=2f5c977f1fe4c5d58ff8c30431e6d7c8")
-        .then(response => response.json())
-        .then(res => {
-          if (this.searchValue) {
-            this.posts = res.results.filter(posts =>
-              posts.name.toLowerCase().includes(this.searchValue.toLowerCase())
-            );
-          } else {
-            this.posts = res.results;
-          }
-        });
-    }
-  },
-  created() {
-    this.getPopularMovies();
-  },
+  mounted: function () {
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=2f5c977f1fe4c5d58ff8c30431e6d7c8")
+      .then(response => response.json())
+      .then(postResponse => {
+        this.posts = postResponse.results;
+      })
+      .catch(error => console.log(error))
+  }
+});
 
-  // computed: {
-  //   getPopularMovies() {
-  //     return this.posts.filter(posts => {
-  //       return posts.title.toLowerCase().includes(this.searchValue.toLowerCase())
-  //     })
-  //   }
-  // }
 
-  // mounted: function () {
-  //   fetch("https://api.themoviedb.org/3/movie/popular?api_key=2f5c977f1fe4c5d58ff8c30431e6d7c8")
-  //     .then(response => response.json())
-  //     .then(postResponse => {
-  //       this.posts = postResponse.results;
-  //     })
-  //     .catch(error => console.log(error))
-  // }
-})
+
+// Tested solutions
+// computed: {
+//   // A computed property that holds only those data that match the searchString.
+//   filteredData: function () {
+//     var search_array = this.item,
+//       searchString = this.searchString;
+
+//     if (!searchString) {
+//       return search_array;
+//     }
+
+//     searchString = searchString.trim().toLowerCase();
+
+//     search_array = search_array.filter(item => {
+//       if (post.name.toLowerCase().indexOf(searchString) !== -1) {
+//         return item;
+//       }
+//     })
+
+//     // Return an array with the filtered data.
+//     return search_array;
+//   }
+// },
+
+// computed: {
+//   filteredMovies: function () {
+//     return this.movies.filter((movie) => {
+//       let lowerMovie = movie.toLowerCase();
+//       let lowerSearchValue = this.searchValue.toLowerCase();
+//       return lowerMovie.includes(lowerSearchValue);
+//     });
+//   }
+// },
+
+// methods: {
+//   getPopularMovies() {
+//     fetch("https://api.themoviedb.org/3/movie/popular?api_key=2f5c977f1fe4c5d58ff8c30431e6d7c8")
+//       .then(response => response.json())
+//       .then(res => {
+//         if (this.searchValue) {
+//           this.posts = res.results.filter(posts =>
+//             posts.name.toLowerCase().includes(this.searchValue.toLowerCase())
+//           );
+//         } else {
+//           this.posts = res.results;
+//         }
+//       });
+//   }
+// },
--- test.js
+++ test.js
@@ -1,131 +1,186 @@
-// // Vue.filter('formatDate', function (d) {
-// //   if (!window.Intl) return d;
-// //   return new Intl.DateTimeFormat('en-US').format(new Date(d));
-// // });
-
-// const app = new Vue({
-//   el: '#app',
-//   data: {
-//     term: '',
-//     results: [],
-//     noResults: false,
-//     searching: false
-//   },
-//   methods: {
-//     search: function () {
-//       this.searching = true;
-//       fetch("https://api.themoviedb.org/3/movie/popular?api_key=2f5c977f1fe4c5d58ff8c30431e6d7c8")
-//         .then(res => res.json())
-//         .then(res => {
-//           this.searching = false;
-//           this.results = res.results;
-//           this.noResults = this.results.length === 0;
-//         });
-//     }
-//   }
-// });
-
-function loadData(event) {
-  event.preventDefault();
-  // only display favourite list if items are there
-  $(".view-fav").on("click", function () {
-    if ($.trim($(".favourite").html()).length) {
-      $(".favourite").fadeToggle();
-      $(".bg").fadeToggle();
-    }
-  });
-
-  let search;
-  // check if no input
-  // set default search to jaws
-  if (!$("input").val()) {
-    search = "bond";
-  } else {
-    search = $("input").val();
-    $("input").val("");
+class Api {
+  constructor(key) {
+    this.key = key;
+    this.baseUrl = 'https://api.themoviedb.org/3';
   }
 
-  // api and end points
-  // create search with user input
-  const api = "&api_key=feb6f0eeaa0a72662967d77079850353";
-  const endpoint = `https://api.themoviedb.org/3/search/movie?query=${search}${api}`;
-  const poster = "https://image.tmdb.org/t/p/w600/";
-
-  let gridHtml = "";
-  let selectedHtml = "";
-
-  $.getJSON(endpoint, function (data) {
-
-    // show film details in pop up
-    $("body").on("click", "li", function (e) {
-      $(".view-fav").fadeToggle();
-      $(".selectedFilm").fadeIn();
-      $(".bg").fadeToggle();
-      // get number of list item
-      // match this the data returned in results
-      // to be able to pull through same info
-      let index = $("li").index(this);
-      let selected = data.results[index];
-      // creating film pop up layout and info
-      gridHtml = `
- <i class="fa fa-times close" aria-hidden="true"></i>
-        <div class="selectedFilm__img">
-                <img class ="selectedFilm__poster" src ="${poster}${selected.poster_path}">
-         </div>
-         <div class="selectedFilm__content">
-            <h2>${selected.title}</h2>
-            <h3>Overview</h3>
-            <p>${selected.overview}</p>
-        
-        <span class ='add-fav'>Add to favourites <i class="fa fa-star" aria-hidden="true"></i></span>
-        </div>`;
-      $(".selectedFilm").html(gridHtml);
-
-      // close the pop up
-      $(".close").on("click", function () {
-        $(".selectedFilm").fadeOut();
-        $(".bg").fadeToggle();
-        $(".view-fav").fadeToggle();
-      });
+  searchMovies(query, page = 1) {
+    const queryString = this.queryString({
+      query,
+      page
     });
-    // if no results display error message
-    if (data.results.length == 0) {
-      $(".error").html("No data found, search again.");
-    }
-    // create the film layout
-    for (let i = 0; i < data.results.length; i++) {
-      const movieResults = data.results[i];
-      gridHtml += `<li>
-        <img src = "${poster}${movieResults.poster_path}">
-    </li>`;
-    }
+    const url = `${this.baseUrl}/search/movie?${queryString}`;
 
-    $(".films").html(gridHtml);
-  });
+    return fetch(url).then(response => response.json());
+  }
 
-  // click of pop up image - add to favourites div
-  $("body").on("click", ".add-fav", function () {
-    // get the img src
-    let favItems = $(".selectedFilm__poster").attr("src");
-    $(".favourite").append(`<img class ="favImg" src ="${favItems}">`);
-    // bug - unsure why? duplicate images being displayed on click.
-    // removed any duplicated src
-    var img = $(".favImg");
-    var used = {};
+  getMovieDetails(id) {
+    const queryString = this.queryString();
+    const url = `${this.baseUrl}/movie/${id}?${queryString}`;
 
-    img.each(function () {
-      var src = $(this).attr("src");
-      if (used[src]) $(this).remove();
-      used[src] = 1;
-    });
-    localStorage.getItem(favItems);
-  });
-  localStorage.setItem(favItems);
+    return fetch(url).then(response => response.json());
+  }
 
+  getGenres() {
+    const queryString = this.queryString();
+    const url = `${this.baseUrl}/genre/movie/list?${queryString}`;
+
+    return fetch(url).then(response => response.json());
+  }
+
+  queryString(obj = {}) {
+    obj.api_key = this.key;
+    obj.language = 'en-US';
+
+    return Object.keys(obj).map(key => `${key}=${encodeURIComponent(obj[key])}`).join('&');
+  }
 }
-// start app
+
+const api = new Api('2f5c977f1fe4c5d58ff8c30431e6d7c8');
 
 
-// use submit to load the data
-const form = $(".form");
-form.submit(loadData);
+const movieComponent = {
+  props: {
+    summary: {
+      default: {},
+    },
+  },
+
+  data() {
+    return {
+      isLoading: false,
+      detailsLoaded: false,
+      details: {},
+    };
+  },
+
+  computed: {
+    description() {
+      if (this.details.overview) {
+        return this.details.overview;
+      }
+
+      return this.truncate(this.summary.overview);
+    },
+
+    posterImage() {
+      return `http://image.tmdb.org/t/p/w185/${this.details.poster_path}`;
+    },
+  },
+
+  methods: {
+    loadDetails() {
+      if (this.detailsLoaded) {
+        return;
+      }
+
+      this.isLoading = true;
+
+      api.getMovieDetails(this.summary.id)
+        .then((response) => {
+          this.isLoading = false;
+          this.detailsLoaded = true;
+          this.details = response;
+        });
+    },
+
+    truncate(str, max = 150) {
+      if (str.length <= max) {
+        return str;
+      }
+
+      return `${str.substr(0, max)}...`;
+    }
+  },
+};
+
+new Vue({
+  el: '.js-instance',
+  name: 'vue-instance',
+
+  components: {
+    movieComponent,
+  },
+
+  data() {
+    return {
+      isLoading: false,
+
+      query: '',
+      page: 1,
+
+      genres: {},
+
+      movies: {
+        results: [],
+        total_pages: 0,
+      },
+    };
+  },
+
+  watch: {
+    query() {
+      if (this.query.length >= 2) {
+        this.searchMovie();
+      }
+    },
+
+    page() {
+      this.searchMovie();
+    },
+  },
+
+  created() {
+    this.searchMovie = _.debounce(this.searchMovie, 500);
+    this.getGenres();
+  },
+
+  methods: {
+
+    getGenres() {
+      this.isLoading = true;
+
+      api.getGenres().then((result) => {
+        this.isLoading = false;
+        this.genres = result.genres.reduce((memo, genre) => {
+          memo[genre.id] = genre.name;
+
+          return memo;
+        }, {});
+      });
+    },
+
+    searchMovie() {
+      this.isLoading = true;
+
+      api.searchMovies(this.query, this.page)
+        .then((response) => {
+          this.isLoading = false;
+
+          response.results.forEach((movie) => {
+            movie.genres = movie.genre_ids.reduce((memo, genreId) => {
+              memo.push({
+                id: genreId,
+                name: this.getGenreName(genreId)
+              });
+
+              return memo;
+            }, []);
+          });
+
+          window.scrollTo(0, 0);
+          this.movies = response;
+        });
+    },
+
+    getGenreName(id) {
+      const genre = this.genres[id];
+
+      if (!genre) {
+        return;
+      }
+
+      return genre;
+    }
+  },
+});
